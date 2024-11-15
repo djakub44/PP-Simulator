1af9165d9501659eb544a341910f371df7f44c65
djakubas <danieljakubas94@gmail.com>
2024-11-15 19:20:39 +0100

    Add project files.

---
+++ Simulator/Creature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator
+{
+    internal class Creature
+    {
+        public string? Name
+        {
+            get { return Name; }
+        }
+
+        private int level;
+        public int Level
+        {
+            get { return level; }
+            set { level = value > 0 ? value : 1; }
+        }
+        
+    }
+}
---
+++ Simulator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Simulator!\n");
+            Creature c = new Creature();
+            Console.WriteLine(c.Name);
+            Console.WriteLine(c.Level);
+        }
+    }
+}
